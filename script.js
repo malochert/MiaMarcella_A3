7220f97533e3c0e5b3a88e93f5b698ceac728a1c
malochert <malochert@student.unimel.edu.au>
2024-06-06 10:59:20 +1000

    new updates to page structures

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function minusPerson() {
-    var peoplequantity = 0;
-    document.getElementById("pn1") = peoplequantity;
+    int(document.getElementById("pn1")) = peoplequantity;
     document.getElementById("pn1") = peoplequantity + 1;
 }
