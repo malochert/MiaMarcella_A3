3c0bec015f8c1bd2cbb035a65a4b1a9302fd0b89
malochert <malochert@student.unimel.edu.au>
2024-06-07 13:10:39 +1000

    updates to shopping cart and payment info

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ function minusPerson() {
     int(document.getElementById("pn1")) = peoplequantity;
     document.getElementById("pn1") = peoplequantity + 1;
 }
+
+function openForm() {
+    document.getElementById("methodform").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("methodform").style.display = "none";
+}
