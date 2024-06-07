857a32ca528b4aedd7e6e524d22d36ee913f56f6
malochert <malochert@student.unimel.edu.au>
2024-06-07 15:34:32 +1000

    shopping cart updates (esp. popup menu)

--- script.js
+++ script.js
@@ -5,10 +5,12 @@ function minusPerson() {
 
 function openForm() {
     document.getElementById("methodform").style.display = "block";
+    document.getElementById("formbg").style.display = "block";
     document.querySelector(".shoppingcart").className = "blurred";
 }
 
 function closeForm() {
     document.getElementById("methodform").style.display = "none";
+    document.getElementById("formbg").style.display = "none";
     document.querySelector(".blurred").className = "shoppingcart";
 }
