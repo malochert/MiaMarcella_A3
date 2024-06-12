bfba4b102fc60e970847a3e4e2119f69a722be56
malochert <malochert@student.unimel.edu.au>
2024-06-12 11:00:24 +1000

    js, html + css changes

--- script.js
+++ script.js
@@ -14,3 +14,9 @@ function closeForm() {
     document.getElementById("formbg").style.display = "none";
     document.querySelector(".blurred").className = "shoppingcart";
 }
+
+function secondButton() {
+    document.getElementById("first_button").style.display = "none";
+    document.getElementById("second_button").style.display = "block";
+    return true;
+}
